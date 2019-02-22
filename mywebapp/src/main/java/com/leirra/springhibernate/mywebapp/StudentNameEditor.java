0a2d5e2246aba10f23da52f0bae5fe5c7b403e88
leste <leste@192.168.1.3>
2019-02-22 23:38:11 +0800

    first commit and comment

---
+++ mywebapp/src/main/java/com/leirra/springhibernate/mywebapp/StudentNameEditor.java
@@ -0,0 +1,20 @@
+package com.leirra.springhibernate.mywebapp;
+
+import java.beans.PropertyEditorSupport;
+
+//must extend propertyeditorsupport when creating custom editor
+// String value will be passed in the URL as path variable, and we’ll bind that value as an ExoticType object which merely
+//keeps the value as an attribute
+public class StudentNameEditor extends PropertyEditorSupport {
+
+	@Override
+	public void setAsText(String studentName) throws IllegalArgumentException {
+		if(studentName.contains("Mr. ")|| studentName.contains("Ms. ")) {
+			setValue(studentName);
+			
+		}else {
+			studentName = "Ms. "+studentName;
+			setValue(studentName);
+		}
+	}
+}
